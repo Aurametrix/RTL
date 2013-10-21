b5e5aa7fa0fdd019824dc97f5b6ac75203a43e98
Irene <irene@aurametrix.com>
2013-10-21 08:05:58 -0700

    test bench to simulate the latch

--- Flip_Flop/latch.v
+++ Flip_Flop/latch.v
@@ -8,3 +8,30 @@ def latch(q, d, g):
             q.next = d
  
     return logic
+
+//simulate
+from random import randrange
+ 
+def test_latch():
+ 
+    q, d, g = [Signal(bool(0)) for i in range(3)]
+ 
+    latch_inst = latch(q, d, g)
+ 
+    @always(delay(7))
+    def dgen():
+        d.next = randrange(2)
+ 
+    @always(delay(41))
+    def ggen():
+        g.next = randrange(2)
+ 
+ 
+    return latch_inst, dgen, ggen
+ 
+def simulate(timesteps):
+    tb = traceSignals(test_latch)
+    sim = Simulation(tb)
+    sim.run(timesteps)
+ 
+simulate(20000)
