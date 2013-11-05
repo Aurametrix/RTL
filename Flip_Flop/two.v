c7ba3e9a2c92506c1e4bd07e62e0222091a1b42e
Irene <irene@aurametrix.com>
2013-11-05 15:26:17 -0800

    two simple flip flops

---
+++ Flip_Flop/two.v
@@ -0,0 +1,22 @@
+// two flip flops (latches)
+// each has two stable states and can be used for data storage
+
+module toplevel(clock,reset);
+  input clock;
+  input reset;
+ 
+  reg flop1;
+  reg flop2;
+ 
+  always @ (posedge reset or posedge clock)
+    if (reset)
+      begin
+        flop1 <= 0;
+        flop2 <= 1;
+      end
+    else
+      begin
+        flop1 <= flop2;
+        flop2 <= flop1;
+      end
+endmodule
