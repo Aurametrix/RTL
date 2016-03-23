c3369ef649d117037a63ae96ce4acc74c82e23b9
Aurametrix <irene@aurametrix.com>
2016-03-22 21:20:03 -0700

    clocked solution

--- ALG/SORT/bubble2.v
+++ ALG/SORT/bubble2.v
@@ -1,15 +1,4 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.all;
-
-package BSTypes is
-  type BSData_Typ is array (0 to 9) of integer range 0 to 127;
-end BSTypes;
-
-package body BSTypes is
-end BSTypes;
-
-
-
+-- Typdefinition BSData_Typ s.o.
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
@@ -17,35 +6,50 @@ use IEEE.NUMERIC_STD.ALL;
 use work.bstypes.all;
 
 entity Bubblesort is
-    Port ( di  : in  BSData_Typ;
-           do  : out BSData_Typ );
+    Port ( di    : in  BSData_Typ;
+           do    : out BSData_Typ;
+           start : in  std_logic;
+           done  : out std_logic;
+           clk   : in  std_logic);
 end Bubblesort;
 
 architecture Behavioral of Bubblesort is
+signal busy     : boolean := FALSE;
+signal din      : BSData_Typ;
+signal n,i      : integer range 0 to din'length;
 begin
 
-   process (di) 
-   variable din  : BSData_Typ;
+   process 
    variable temp : integer;
-   variable n    : integer;
-   variable oncemore : boolean;
    begin
-      din      := di;
-      n        := din'length;
-      oncemore := TRUE;
-      while oncemore = TRUE and n > 1 loop
-         oncemore := FALSE;
-         for i in 0 to din'length-2 loop
-            if din(i) > din(i+1) then 
-               temp     := din(i);   -- Dreieckstausch
-               din(i)   := din(i+1);
-               din(i+1) := temp;
-               oncemore := TRUE;
+      wait until rising_edge(clk);
+      if (busy=FALSE) then
+         if (start='1') then
+            din  <= di;
+            busy <= TRUE;
+            n    <= 0;
+            i    <= 0;
+         end if;
+      else
+         if (n < din'length) then
+            if (i<din'length-1) then
+               if din(i) > din(i+1) then
+                  temp     := din(i);   -- Dreieckstausch
+                  din(i)   <= din(i+1);
+                  din(i+1) <= temp;
+               end if;
+               i <= i+1;
+            else
+               i <= 0;
+               n <= n+1;
             end if;
-         end loop;
-         n := n-1;
-      end loop;
-      do <= din;
+         else
+            busy <= FALSE;       
+            do   <= din;
+         end if;
+      end if;
    end process;
    
+   done <= '1' when start='0' and busy=FALSE else '0';
+
 end Behavioral;
